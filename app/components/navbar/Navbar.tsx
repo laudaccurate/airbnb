6fa10eaa14f7e712516a5e3e6dde3857cc073d91
Laud Gilbert <laudaccurate@gmail.com>
2023-06-02 19:39:51 +0000

    search component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,17 @@
 import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
 
 const Navbar = () => {
 	return (
 		<div className="fixed w-full bg-white z-10 shadow-sm">
 			<div className="py-4 border-b-[1px]">
-				<Container>Header</Container>
+				<Container>
+					<div className="flex flex-row items-center justify-between gap-3 md:gap-4">
+						<Logo />
+						<Search />
+					</div>
+				</Container>
 			</div>
 		</div>
 	);
