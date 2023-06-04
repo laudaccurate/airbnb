95589a4affe02b2c51eabcdeae56539bdd9cedb9
Laud Gilbert <laudaccurate@gmail.com>
2023-06-04 23:25:22 +0000

    modal laout and actions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Modal from "./components/modals/Modal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
@@ -19,6 +20,12 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={font.className}>
+				<Modal
+					isOpen
+					title="Login"
+					actionLabel="Submit"
+					secondaryActionLabel="Rgister"
+				/>
 				<Navbar />
 				{children}
 			</body>
