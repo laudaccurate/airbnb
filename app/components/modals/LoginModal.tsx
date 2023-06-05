9f49156ca6515a5ba962029bf775c7fcd0a7466f
Laud Gilbert <laudaccurate@gmail.com>
2023-06-05 14:31:39 +0000

    login modal done

---
+++ app/components/modals/LoginModal.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useRegiterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
+import Button from "../Button";
+
+const LoginModal = () => {
+	const router = useRouter();
+	const loginModal = useLoginModal();
+	const registerModal = useRegiterModal();
+	const [isLoading, setIsLoading] = useState(false);
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FieldValues>({
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	const onSubmit: SubmitHandler<FieldValues> = (data) => {
+		setIsLoading(true);
+
+		signIn("credentials", { ...data, redirect: false }).then((callback) => {
+			setIsLoading(false);
+
+			if (callback?.ok) {
+				toast.success("Logged In");
+				router.refresh();
+				loginModal.onClose();
+			}
+
+			if (callback?.error) {
+				toast.error(callback.error);
+			}
+		});
+	};
+
+	const bodyContent = (
+		<div className="flex flex-col gap-4">
+			<Heading title="Welcome back" subtitle="Login to your account!" />
+			<Input
+				register={register}
+				id="email"
+				label="Email"
+				disabled={isLoading}
+				errors={errors}
+				required
+			/>
+			<Input
+				register={register}
+				id="password"
+				type="password"
+				label="Password"
+				disabled={isLoading}
+				errors={errors}
+				required
+			/>
+		</div>
+	);
+
+	const footerContent = (
+		<div className="flex flex-col gap-4 mt-3">
+			<hr />
+			<Button
+				outline
+				label="Continue with Google"
+				icon={FcGoogle}
+				onClick={() => {}}
+			/>
+			<Button
+				outline
+				label="Continue with Github"
+				icon={AiFillGithub}
+				onClick={() => {}}
+			/>
+			<div className="text-neutral-500 text-center mt-4 font-light">
+				<div className="flex flex-row justify-center items-center gap-2">
+					<div>Already have an account?</div>
+					<div
+						onClick={loginModal.onClose}
+						className="text-neutral-800 cursor-pointer hover:underline font-semibold"
+					>
+						Log in
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+
+	return (
+		<Modal
+			disabled={isLoading}
+			isOpen={loginModal.isOpen}
+			title="Login"
+			actionLabel="Continue"
+			onClose={loginModal.onClose}
+			onSubmit={handleSubmit(onSubmit)}
+			body={bodyContent}
+			footer={footerContent}
+		/>
+	);
+};
+
+export default LoginModal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
 
 export const metadata = {
 	title: "Airbnb",
@@ -23,6 +24,7 @@ export default function RootLayout({
 			<body className={font.className}>
 				<ToasterProvider />
 				<RegisterModal />
+				<LoginModal />
 				<Navbar />
 				{children}
 			</body>
