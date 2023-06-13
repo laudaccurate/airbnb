760529804af415933b95f2d24b44cc6b91160c02
Laud Gilbert <laudaccurate@gmail.com>
2023-06-13 13:30:32 +0000

    getting current user and logout

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -4,8 +4,13 @@ import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import { SafeUser } from "@/app/types";
 
-const Navbar = () => {
+interface NavbarProps {
+	currentUser?: SafeUser | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
 	return (
 		<div className="fixed w-full bg-white z-10 shadow-sm">
 			<div className="py-4 border-b-[1px]">
@@ -13,7 +18,7 @@ const Navbar = () => {
 					<div className="flex flex-row items-center justify-between gap-3 md:gap-4">
 						<Logo />
 						<Search />
-						<UserMenu />
+						<UserMenu currentUser={currentUser} />
 					</div>
 				</Container>
 			</div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Nunito } from "next/font/google";
 import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
 	title: "Airbnb",
@@ -14,18 +15,20 @@ const font = Nunito({
 	subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const currentUser = await getCurrentUser();
+
 	return (
 		<html lang="en">
 			<body className={font.className}>
 				<ToasterProvider />
 				<RegisterModal />
 				<LoginModal />
-				<Navbar />
+				<Navbar currentUser={currentUser} />
 				{children}
 			</body>
 		</html>
