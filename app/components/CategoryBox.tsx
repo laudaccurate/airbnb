73ab77a959c2afdbe92991eca97a0d7b8c3b643d
Laud Gilbert <laudaccurate@gmail.com>
2023-06-17 11:35:01 +0000

    reset query params

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -49,9 +49,9 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
 	return (
 		<div
 			onClick={handleClick}
-			className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
+			className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 hover:border-b-neutral-800 transition cursor-pointer ${
 				selected ? "border-b-neutral-800" : "border-transparent"
-			} ${selected ? "text-neutral-800" : "text-neutral-500"}`}
+			} ${selected ? "text-neutral-800" : "text-neutral-600"}`}
 		>
 			<Icon size={26} />
 			<div className="font-medium text-sm">{label}</div>
