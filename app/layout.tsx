e2bea6c54087a92eb494300df53cf1b4bfb153d2
Laud Gilbert <laudaccurate@gmail.com>
2023-06-17 12:10:20 +0000

    rent modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Nunito } from "next/font/google";
 import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "./components/modals/RentModal";
 
 export const metadata = {
 	title: "Airbnb",
@@ -28,6 +29,7 @@ export default async function RootLayout({
 				<ToasterProvider />
 				<RegisterModal />
 				<LoginModal />
+				<RentModal />
 				<Navbar currentUser={currentUser} />
 				{children}
 			</body>
