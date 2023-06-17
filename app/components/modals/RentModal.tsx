8fe0f9253db92473edfa49b6594d965ad771a7c9
Laud Gilbert <laudaccurate@gmail.com>
2023-06-17 14:55:27 +0000

    selecting location

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,6 +7,7 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
 
 enum STEPS {
 	CATEGORY = 0,
@@ -44,6 +45,7 @@ const RentModal = () => {
 	});
 
 	const category = watch("category");
+	const location = watch("location");
 
 	const setCustomValue = (id: string, value: any) => {
 		setValue(id, value, {
@@ -101,11 +103,28 @@ const RentModal = () => {
 		</div>
 	);
 
+	if (step === STEPS.LOCATION) {
+		bodyContent = (
+			<div className="flex flex-col gap-8">
+				<Heading
+					title="Where is your place located?"
+					subtitle="Help guests find you!"
+				/>
+				<CountrySelect
+					value={location}
+					onChange={(value) => {
+						setCustomValue("location", value);
+					}}
+				/>
+			</div>
+		);
+	}
+
 	return (
 		<Modal
 			isOpen={rentModal.isOpen}
 			onClose={rentModal.onClose}
-			onSubmit={rentModal.onClose}
+			onSubmit={onNext}
 			actionLabel={actionLabel}
 			secondaryActionLabel={secondaryActionLabel}
 			secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
